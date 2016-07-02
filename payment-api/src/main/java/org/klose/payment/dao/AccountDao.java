5fff847a6908c4e4c1140d097ab97b7a5faf067d
klose911 <klose911@gmail.com>
2016-07-02 18:44:37 +0800

    replace spring boot with tranditonal spring mvn because spring boot does not support jsp well

--- payment-api/src/main/java/org/klose/payment/dao/AccountDao.java
+++ payment-api/src/main/java/org/klose/payment/dao/AccountDao.java
@@ -10,12 +10,12 @@ import java.util.List;
 public interface AccountDao extends CrudRepository<AccountPO, Long>{
 	
 	@Query("select acc.accountNo, acc.name, acc.merchantNo, acc.merchantName, acc.statusId, acc.type, acc.useType " +
-			" from com.ebao.insurance.paymentservice.po.AccountPO acc " +
+			" from #{#entityName} acc " +
 			" where acc.type = :type and acc.useType = 1 and acc.statusId = 1")
 	List<Object[]> findByPaymentType(@Param("type") Integer type);
 	
 	@Query("select acc.accountNo, acc.name, acc.merchantNo, acc.merchantName, acc.statusId, acc.type, acc.useType  " +
-			" from com.ebao.insurance.paymentservice.po.AccountPO acc " +
+			" from #{#entityName} acc " +
 			" where acc.useType = 1 and acc.statusId = 1")
 	List<Object[]> getAllEffectiveAccounts();
 	
