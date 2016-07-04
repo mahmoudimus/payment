4e6b6de06fa0642a4335d399c52f94deddbb424b
klose911 <klose911@gmail.com>
2016-07-04 23:36:59 +0800

    move spring configuration file from server module to runtime model and successfully run first junit test case

--- payment-api/src/main/java/org/klose/payment/server/service/TransactionDataService.java
+++ payment-api/src/main/java/org/klose/payment/server/service/TransactionDataService.java
@@ -6,16 +6,16 @@ import org.klose.payment.server.bo.PaymentResult;
 
 public interface TransactionDataService {
 
-	public PaymentResult findPaymentByOrderNo(String orderNo);
-	
-	public PaymentResult findPaymentByTransactionId(Long transId);
-	
-	public Long createTransactionFromBillingData(BillingData bill);
-	
-	public String processReturnURL(Long transId, String returnURL);
-	
-	public PaymentResult findSuccessfulPaymentByBizNo(String bizNo);
-	
-	public void updatePaymentResult(
-			Long transId, boolean isSuccess, String payId, String notifyMsg);
+    PaymentResult findPaymentByOrderNo(String orderNo);
+
+    PaymentResult findPaymentByTransactionId(Long transId);
+
+    Long createTransactionFromBillingData(BillingData bill);
+
+    String processReturnURL(Long transId, String returnURL);
+
+    PaymentResult findSuccessfulPaymentByBizNo(String bizNo);
+
+    public void updatePaymentResult(
+            Long transId, boolean isSuccess, String payId, String notifyMsg);
 }
