b6bbec6b6245874e23f93ec887e16d254b4d23ae
klose911 <klose911@gmail.com>
2016-10-02 17:56:40 +0800

    java doc

--- payment-runtime/src/main/java/org/klose/payment/integration/alipay/config/AlipayConstant.java
+++ payment-runtime/src/main/java/org/klose/payment/integration/alipay/config/AlipayConstant.java
@@ -5,9 +5,9 @@ public interface AlipayConstant {
     String INPUT_CHARSET = "utf-8";
     // 签名方式
     String SIGN_TYPE = "MD5";
-
+    //PC页面支付集成
     String SERVICE_ALIPAY_GATEWAY = "create_direct_pay_by_user";
-
+    //手机页面支付集成
     String SERVICE_ALIPAY_WAP_GATEWAY = "alipay.wap.create.direct.pay.by.user";
 
     Integer DEFAULT_PAYMENT_TYPE = 1;
--- payment-server/src/main/java/org/klose/payment/server/prepare/PaymentIntegrationService.java
+++ payment-server/src/main/java/org/klose/payment/server/prepare/PaymentIntegrationService.java
@@ -5,11 +5,12 @@ import org.klose.payment.bo.BillingData;
 
 public interface PaymentIntegrationService {
 	/**
-	 *
-	 * @param businessNumber
+	 * 准备订单数据
+	 * @param businessNumber 货物标识符
 	 * @return
      */
 	BillingData prepareBillingData(String businessNumber);
-	
+
+
 	void saveTransactionId(String businessNumber, Long transactionId);
 }
