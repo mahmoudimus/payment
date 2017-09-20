a0663b8846e5eb89ee3998b1678becaea6697d66
klose911 <klose911@gmail.com>
2017-09-20 19:16:10 +0800

    init payment form

---
+++ payment-ui/src/components/PaymentForm.vue
@@ -0,0 +1,26 @@
+<<template>
+  <div>
+        bizNo<input v-model="bizNo"/>
+        accountNo<input v-model="accountNo"/>
+	    <button @click="submitPay"> submit</button>
+   <div>
+        <p>{{ msg }} transaction</p>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaymentForm',
+  data () {
+    return {
+      msg: '2'
+    }
+  },
+  methods: {
+    submitPay () {
+      this.msg = 100
+    }
+  }
+}
+</script>
