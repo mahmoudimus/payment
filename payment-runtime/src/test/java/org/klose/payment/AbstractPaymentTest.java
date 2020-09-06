401fe085d282262e1b61ded9c12e422ba3a04432
klose911 <klose911@gmail.com>
2020-09-06 10:23:56 +0800

    upgrade junit & spring-data-jpa

--- payment-runtime/src/test/java/org/klose/payment/AbstractPaymentTest.java
+++ payment-runtime/src/test/java/org/klose/payment/AbstractPaymentTest.java
@@ -1,9 +1,9 @@
 package org.klose.payment;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath*:spring-payment-test.xml" })
-@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
-@Transactional
+@Transactional(transactionManager = "transactionManager")
+@Commit
 public abstract class AbstractPaymentTest {
 }
